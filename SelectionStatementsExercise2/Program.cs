9d9670bee787319fdf83836a6fd20878a5bef3d2
Jonathan Meza <jonathansyear92@gmail.com>
2025-01-07 16:44:13 -0800

    School Subject Exercise

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,37 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            FavSubject();
+        }
+
+        public static void FavSubject()
+        {
+            Console.WriteLine("What is your favorite school subject?");
+            string subject = Console.ReadLine();
+
+            switch (subject.ToLower())
+            {
+                case "math":
+                    Console.WriteLine("I do not prefer math");
+                    break;
+                case "science":
+                    Console.WriteLine("Science rules!");
+                    break;
+                case "pe":
+                case "p.e.":
+                case "physical education":
+                    Console.WriteLine("P.E. is fun");
+                    break;
+                case "history":
+                    Console.WriteLine("History is my favorite too!");
+                    break;
+                case "geography":
+                    Console.WriteLine("I like to study that as well");
+                    break;
+                default:
+                    Console.WriteLine("I like that too");
+                    break;
+            }
         }
     }
 }
